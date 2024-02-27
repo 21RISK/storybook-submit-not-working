4ff3af30bf7fb0a23f35154d1e4ddcf48fa18dad
Alex Bjørlig <alexbjorlig@icloud.com>
2024-02-27 09:30:06 +0100

    added a button to test if cmpt could submit

--- src/stories/Button.svelte
+++ src/stories/Button.svelte
@@ -4,7 +4,6 @@
 	import { userSchema } from './schema';
 	import { zod } from 'sveltekit-superforms/adapters';
 
-
   const formData = defaults({name: 'some name'}, zod(userSchema));
 
   const spForm = superForm(formData, {
@@ -14,6 +13,7 @@
   })
   const {enhance} = spForm;
 
+  let formElement: HTMLFormElement;
 </script>
 
 <form id="form-1" use:enhance method="POST">
@@ -21,8 +21,9 @@
   <button>Submit form with use:enhance</button> <!-- Nothing seems to happen -->
 </form>
 
-<form id="form-2" method="POST" style="margin-top: 20px;">
+<form id="form-2" method="POST" style="margin-top: 20px;" bind:this={formElement}>
   <input type="text" name="name" value="Some text"/>
   <button>Submit form, no enhance</button> <!-- can submit -->
 </form>
 
+<button type="button" on:click={() => formElement.requestSubmit()}>Submit form, no enhance also works</button>
