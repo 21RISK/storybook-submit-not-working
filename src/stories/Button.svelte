6d4d1d46c08a3a778ad3fddf6583852780977ec3
Alex Bjørlig <alexbjorlig@icloud.com>
2024-02-27 09:22:21 +0100

    initial commit

---
+++ src/stories/Button.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import './button.css';
+	import { defaults, superForm } from 'sveltekit-superforms';
+	import { userSchema } from './schema';
+	import { zod } from 'sveltekit-superforms/adapters';
+
+
+  const formData = defaults({name: 'some name'}, zod(userSchema));
+
+  const spForm = superForm(formData, {
+    onSubmit: (data) => {
+      console.log('Form submitted', data);
+    }
+  })
+  const {enhance} = spForm;
+
+</script>
+
+<form id="form-1" use:enhance method="POST">
+
+  <input type="text" name="name" value="Some text"/>
+
+  <button>Submit form with use:enhance</button>
+</form>
+
+<form id="form-2" method="POST" style="margin-top: 20px;">
+
+  <input type="text" name="name" value="Some text"/>
+
+  <button>Submit form, no enhance</button>
+</form>
+
