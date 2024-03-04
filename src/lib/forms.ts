aad2a981563d4a123dd35ca4d3a38e942b9ad3cf
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-04 18:57:02 +0100

    WIP: Improved enhance function

---
+++ src/lib/forms.ts
@@ -0,0 +1,29 @@
+import type { SubmitFunction } from '@sveltejs/kit';
+
+export type EnhanceDetails = {
+	formElement: HTMLFormElement;
+	submitFunction: SubmitFunction;
+	submitEvent: SubmitEvent;
+};
+
+export function enhance(formElement: HTMLFormElement, submitFunction: SubmitFunction) {
+	const listener = (e: Event) => {
+		const event = new CustomEvent('storybook:enhance', {
+			detail: {
+				formElement,
+				submitFunction,
+				submitEvent: e
+			}
+		});
+		window.dispatchEvent(event);
+	};
+	formElement.addEventListener('submit', listener);
+	return {
+		destroy() {
+			formElement.removeEventListener('submit', listener);
+		}
+	};
+}
+
+export function applyAction() {}
+export function deserialize() {}
